03c2e0463fbc2667a0bc9dc96fcdf9ac600d00e2
Jake Stenger <jstenger@ucsb.edu>
2025-04-19 19:25:43 -0700

    add github link, fix title

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/kalshi-dash',
   images: {
     unoptimized: true,
   },
+  // Conditionally apply settings for production build (GitHub Pages)
+  ...(process.env.NODE_ENV === 'production' ? {
+    output: 'export',
+    basePath: '/kalshi-dash',
+  } : {})
 };
 
 module.exports = nextConfig; 
